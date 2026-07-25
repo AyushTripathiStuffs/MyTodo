89cdbabf533675aa7ce9c4e9ef724578732f8f45
Ayush Tripathi <ayushtripathipersonal@gmail.com>
2026-07-26 01:43:06 +0530

    Second Commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/todo'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
