178edf65aa0e6f7e551148d0d638e87e526ed44b
Ayush Tripathi <ayushtripathipersonal@gmail.com>
2026-07-26 02:02:51 +0530

    Fourth Commit

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+with app.app_context():
+    db.create_all()
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
